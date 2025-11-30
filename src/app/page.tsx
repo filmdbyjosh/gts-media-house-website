9c9ffa02099c138a1a2c35926ba037475fce41c9
Josh Weiss <joshweiss@Joshs-MacBook-Pro.local>
2025-11-30 14:37:42 -0700

    Added Image import for hero logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import React, {
 } from "react";
 import { motion } from "framer-motion";
 import { SkeletonCameraIcon } from "../components/SkeletonCameraIcon";
+import Image from "next/image";
 
 /* MAIN PAGE */
 
